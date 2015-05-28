e95132db42d694d8e34d2ccd9eb7059bb4b5c6de
mamaker <madhuvasudevan@hotmail.com>
2015-05-27 23:59:48 -0500

    Push 1

---
+++ ReadingAndWritingUsingTwoObjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingAndWritingUsingTwoObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
