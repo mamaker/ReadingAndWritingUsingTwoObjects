873c63884c573f99ce9e472a0820b10d3c32f7ea
mamaker <madhuvasudevan@hotmail.com>
2015-05-28 00:05:34 -0500

    Done!

--- ReadingAndWritingUsingTwoObjects/Program.cs
+++ ReadingAndWritingUsingTwoObjects/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ReadingAndWritingUsingTwoObjects
 {
@@ -10,6 +11,26 @@ namespace ReadingAndWritingUsingTwoObjects
     {
         static void Main(string[] args)
         {
+            //string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string folder = @"C:\MyOwnApps\GitLoc\HeadFirstCsharp\WorkFolder"; 
+            StreamReader reader =
+            new StreamReader(folder + @"\secret_plan.txt");
+            StreamWriter writer =
+            new StreamWriter(folder + @"\emailToCaptainAmazing.txt");
+            writer.WriteLine("To: CaptainAmazing@objectville.net");
+            writer.WriteLine("From: Commissioner@objectville.net");
+            writer.WriteLine("Subject: Can you save the day... again?");
+            writer.WriteLine();
+            writer.WriteLine("We’ve discovered the Swindler’s plan:");
+            while (!reader.EndOfStream)
+            {
+                string lineFromThePlan = reader.ReadLine();
+                writer.WriteLine("The plan -> " + lineFromThePlan);
+            }
+            writer.WriteLine();
+            writer.WriteLine("Can you help us?");
+            writer.Close();
+            reader.Close();
         }
     }
 }
